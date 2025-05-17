61a23d6e0f56cf0d8190758e0f4eefad9901b7ac
fknlatw <argat2480@gmail.com>
2025-05-17 16:32:09 +0600

    starting_ts_server

---
+++ backend/dist/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 9999;
+app.listen(PORT, () => {
+    console.log("server hosted on http://localhost:" + PORT);
+});
