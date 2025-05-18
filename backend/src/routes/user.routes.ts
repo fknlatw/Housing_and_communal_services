87d211092e90a009f5c3c53b088e810acfc9854b
fknlatw <argat2480@gmail.com>
2025-05-18 22:46:09 +0600

    added_user_routes_and_controller

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,8 +3,8 @@ import userControllers from "../controllers/user.controllers.js";
 
 const router = Router();
 
-router.get("/signup", userControllers.signup);
-router.get("/login", userControllers.login);
-router.get("/logout", userControllers.logout);
+router.post("/signup", userControllers.signup);
+router.post("/login", userControllers.login);
+router.post("/logout", userControllers.logout);
 
 export default router;
